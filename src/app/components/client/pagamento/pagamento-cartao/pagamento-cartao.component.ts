ed4dde0379dfaa170fbc1392690a8356ffb99ed0
João Victor <joaovictorpfr@gmail.com>
2024-06-03 20:57:34 -0300

    feat: card-payment component

---
+++ src/app/components/client/pagamento/pagamento-cartao/pagamento-cartao.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagamento-cartao',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagamento-cartao.component.html',
+  styleUrls: ['./pagamento-cartao.component.css']
+})
+export class PagamentoCartaoComponent {
+
+}
