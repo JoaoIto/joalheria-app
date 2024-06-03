b9cf3a160caf7f40672db8dadd851b4392237114
João Victor <joaovictorpfr@gmail.com>
2024-06-03 20:57:20 -0300

    feat: payment component

---
+++ src/app/components/client/pagamento/pagamento.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagamento',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagamento.component.html',
+  styleUrls: ['./pagamento.component.css']
+})
+export class PagamentoComponent {
+
+}
